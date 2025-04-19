42c099e84b83405b3c18450aa118a1b0605111a4
Dr292 <maxkituto@gmail.com>
2025-04-20 01:35:54 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255697608274"
-global.ownername = "𝐷𝛥𝛮𝛮𝑌"
-global.ownernumber = '255697608274'  //owner number 2
+global.ownernomer = "254746083017"
+global.ownername = "max john"
+global.ownernumber = '25474608301'  //owner number 2
 //put your phone number above
 //let the owner as Danny or you can just put your name
 
